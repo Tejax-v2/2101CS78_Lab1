bbd0dc82bdb05d3494aae841cee9ffd7a5d68a47
Tejax-v2 <tejastupke74@gmail.com>
2022-07-27 21:10:57 +0530

    Subtraction operation added and Exit operation fixed

--- Calculator.c
+++ Calculator.c
@@ -4,11 +4,16 @@ int add(int x,int y){
     return x+y;
 }
 
+int subtract(int x,int y){
+    return x-y;
+}
+
 int main(){
 
     while(1){
         printf("\n1) Addition\n");
-        printf("2) Exit\n");
+        printf("2) Subtraction\n");
+        printf("3) Exit\n");
         printf("\nSelect your operation:-\n");
         int x;
         scanf("%d",&x);
@@ -21,7 +26,18 @@ int main(){
             scanf("%d",&b);
             printf("Sum of %d & %d is: %d\n",a,b,add(a,b)); 
         }
-        
+        else if(x==2){
+            int a,b;
+            printf("Enter first number: \n");
+            scanf("%d",&a);
+            printf("Enter second number \n");
+            scanf("%d",&b);
+            printf("Difference of %d & %d is: %d\n",a,b,subtract(a,b)); 
+        }
+        else if(x==3){
+            printf("Calculator Stopped");
+            return 0;
+        }
         else{
             printf("Invalid Input\n");
         }
