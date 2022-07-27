de8da3ebbe4bac0f083958b70fc68d112cbfb02d
Tejax-v2 <tejastupke74@gmail.com>
2022-07-27 21:14:44 +0530

    Multiplication Operation added

--- Calculator.c
+++ Calculator.c
@@ -8,12 +8,17 @@ int subtract(int x,int y){
     return x-y;
 }
 
+int multiply(int x,int y){
+    return x*y;
+}
+
 int main(){
 
     while(1){
         printf("\n1) Addition\n");
         printf("2) Subtraction\n");
-        printf("3) Exit\n");
+        printf("3) Multiplication\n");
+        printf("4) Exit\n");
         printf("\nSelect your operation:-\n");
         int x;
         scanf("%d",&x);
@@ -35,6 +40,14 @@ int main(){
             printf("Difference of %d & %d is: %d\n",a,b,subtract(a,b)); 
         }
         else if(x==3){
+            int a,b;
+            printf("Enter first number: \n");
+            scanf("%d",&a);
+            printf("Enter second number \n");
+            scanf("%d",&b);
+            printf("Multiplication of %d & %d is: %d\n",a,b,multiply(a,b)); 
+        }
+        else if(x==4){
             printf("Calculator Stopped");
             return 0;
         }
